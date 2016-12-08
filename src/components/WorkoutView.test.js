48b6587220f4f11cf6976111025f180fbdc412c4
Remon Georgy <remon.sherin@gmail.com>
2016-12-08 05:14:36 -0800

    Initial attempt

--- src/components/WorkoutView.test.js
+++ src/components/WorkoutView.test.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WorkoutView from './WorkoutView'
 
-it('Just renders without throwing', () => {
-  expect(true).toEqual(true)
-  
-  const div = document.createElement('div');
-  const props = {
-    name: 'Annie',
-    scoring: 'rounds',
-    clusters: []
-  }
-  ReactDOM.render(<WorkoutView {...props} />, div);
-})
+import 'react-native';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import WorkoutView from './WorkoutView'
+import renderer from 'react-test-renderer';
+
+// it('Just renders without throwing', () => {
+//   expect(true).toEqual(true)
+//
+//   const props = {
+//     name: 'Annie',
+//     scoring: 'rounds',
+//     clusters: []
+//   }
+//
+//   const component = renderer.create(<WorkoutView {...props} />);
+// //  const tree = component.toJSON();
+// //  expect(tree).toMatchSnapshot();
+// })

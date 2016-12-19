f6a17c90767f8388d2971fd518bacba07a698399
Remon Georgy <remon.sherin@gmail.com>
2016-12-18 18:32:18 -0800

    Purify WorkoutsListView component

--- src/components/WorkoutsListView.js
+++ src/components/WorkoutsListView.js
@@ -6,25 +6,19 @@ import React, { Component, PropTypes } from 'react';
 import { ScrollView } from 'react-native';
 import WorkoutView from './WorkoutView';
 
-export default class WorkoutsListView extends Component {
-  static propTypes = {
-    workouts: PropTypes.array.isRequired
-  }
-  
-  render() {
-    const { workouts } = this.props
-    const rows = workouts.map((workout, i) => {
-      return <WorkoutView {...workout} key={i}/>
-    });
+const WorkoutsListView = ({ workouts }) => {
+  const rows = workouts.map((workout, i) => {
+    return <WorkoutView {...workout} key={i}/>
+  });
 
-    return (
-      <ScrollView>
-        {rows}
-      </ScrollView>
-    );
-  }
+  return (
+    <ScrollView>
+      {rows}
+    </ScrollView>
+  );
 }
-
 WorkoutsListView.propTypes = {
   workouts: PropTypes.array.isRequired
 }
+
+export default WorkoutsListView
